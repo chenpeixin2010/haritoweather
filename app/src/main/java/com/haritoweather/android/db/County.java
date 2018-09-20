f5871f17ce46061ca7caba2b2733998df28d1f1f
Harito <645268403@qq.com>
2018-09-20 10:48:57 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/haritoweather/android/db/County.java
+++ app/src/main/java/com/haritoweather/android/db/County.java
@@ -1,4 +1,42 @@
 package com.haritoweather.android.db;
 
-public class County {
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport{
+    private int id;
+    private String countyName;  //记录县的名字
+    private String weatherId;   //记录县对应的天气id
+    private int cityid;         //记录当前县所属市的id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityid() {
+        return cityid;
+    }
+
+    public void setCityid(int cityid) {
+        this.cityid = cityid;
+    }
 }
